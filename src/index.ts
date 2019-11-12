481a0eea85fd4c829ed630050cc95c142b2302e2
Ronald Dijks <ronalddijks@protonmail.com>
2019-11-12 21:08:18 +0100

    add png loading

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("hello world");
+const canvas = document.getElementById("canvas-root")! as HTMLCanvasElement;
+const context = canvas.getContext("2d")!;
+
+import machine from "./img/CopieMachine.png";
+
+const drawing = new Image();
+drawing.src = machine;
+drawing.onload = (): void => {
+  context.drawImage(drawing, 0, 0);
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,43 @@
-const path = require('path');
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.ts',
+  mode: "development",
+  entry: "./src/index.ts",
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
+        use: "ts-loader",
+        exclude: /node_modules/
       },
-    ],
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader"
+          }
+        ]
+      }
+    ]
   },
   resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ],
+    extensions: [".tsx", ".ts", ".js"]
   },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000,
+    open: true
+  }
 };
